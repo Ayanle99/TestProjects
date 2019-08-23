bbe4209eb507868fd024223555ccd3241718001e
Ayanle Yusuf <ayanle226@gmail.com>
2019-08-23 00:34:44 -0500

    Added a way to search a city, in my case Minneapolis, and post the title and the content on to the console

--- read_wikipedia.py
+++ read_wikipedia.py
@@ -1,2 +1,14 @@
-import wikipedia
-print(wikipedia.summary("Java Language"))
+import wikipedia as wiki
+
+mpls = wiki.page("Minneaplis")
+
+link = mpls.url
+title = mpls.title
+
+
+content = mpls.content
+
+print("Link (URL) -> "+link + "\n")
+print("Title -> " +title + "\n\n")
+
+print("\t\t"+content)
